e435c16cdab9f10a0b419c597d42116d54e8a18c
RAUL RIVERO RUBIO <5000928902@mwccpod10-06.local>
2020-03-05 16:17:11 -0800

    - Added another ViewController with its NavigationController. - Created Cocoa Touch class "LoginViewController" and "FeedViewController" linked to its respective controllers.

---
+++ Parstagram/FeedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedViewController.swift
+//  Parstagram
+//
+//  Created by RAUL RIVERO RUBIO on 3/5/20.
+//  Copyright © 2020 codepath-cit238b-spring2020. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
