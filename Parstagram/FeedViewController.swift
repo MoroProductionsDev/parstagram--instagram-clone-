182151a6f61bcf655a0e3a5679a0fc97405ecee7
RAUL RIVERO RUBIO <5000928902@mwccpod10-06.local>
2020-03-07 15:01:27 -0800

    - Added the logic for the displaying the cell image view, labels in the table view.

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -7,15 +7,61 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var tableView: UITableView!
-
+    
+    var posts = [PFObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let query = PFQuery(className: "Posts")
+        
+        // Since author is a pointer to another object, it has to fetch the data
+        // to disply it
+        query.includeKey("author")
+        query.limit = 20
+        
+        // query sequence
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!         // set the array equal to fetched data
+                self.tableView.reloadData() // reload the data in the tableview
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let singlePost = posts[indexPath.row]
+        let user = singlePost["author"] as! PFUser
+        
+        // AlamofireImage
+        let imageFile = singlePost["image"] as! PFFileObject
+        let urlString =  imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.usernameLabel.text = user.username
+        cell.captionLabel.text = (singlePost["caption"] as! String)
+        cell.photoImageView.af_setImage(withURL: url)
+        
+        return cell
     }
     
 
