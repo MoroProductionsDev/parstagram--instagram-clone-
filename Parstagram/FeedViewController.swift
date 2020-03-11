ac5f3c8865143e221c29ceb742dc5f286b843f2a
RAUL RIVERO RUBIO <5000928902@mwccpod10-06.local>
2020-03-10 19:59:51 -0700

    - Created a logic to add "fake" comments base on select event of the image view.

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -64,6 +64,26 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post = posts[indexPath.row]
+        let comment = PFObject(className: "Comments")
+        
+        comment["text"] = "Random comment"
+        comment["post"] = post
+        comment["author"] = PFUser.current()
+        
+        // add comment to "comments" array
+        post.add(comment, forKey: "comments")
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                print("Comment saved")
+            } else {
+                print("Error saving comment")
+            }
+        }
+    }
+    
 
     /*
     // MARK: - Navigation
